893cbf7c89d4cf573eb118dcc6288347425484dc
Jacob-Beckstett <126559022+Jacob-Beckstett@users.noreply.github.com>
2025-01-24 23:25:10 +0100

    Added Post Behaviour

---
+++ GGJ2025/Assets/Scripts/Posts/Content.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Posts
+{
+    [Serializable]
+    public struct Content
+    {
+        public string contentText;
+        public Sprite contentImage;
+
+        public enum ContentType
+        {
+            TextPost,
+            ImagePost
+        }
+    }
+}
