9ec14fef76dff33e51b60423e4cc9da27cd831e8
Jacob-Beckstett <126559022+Jacob-Beckstett@users.noreply.github.com>
2025-01-25 01:45:36 +0100

    Updated Comments to work via UI

--- GGJ2025/Assets/Scripts/Posts/Comments/CommentBehaviour.cs
+++ GGJ2025/Assets/Scripts/Posts/Comments/CommentBehaviour.cs
@@ -1,6 +1,25 @@
+using Posts;
 using UnityEngine;
 
 public class CommentBehaviour : BasePost
 {
+    enum flag{FakeNews, SexBot, HateSpeech, Spam, Scam, Child}
     
+    private void Start() {
+        names.text = userNickname + " @" + userName;
+        userIcon.image = icon;
+        switch (type)
+        {
+            case Content.ContentType.TextPost:
+                contentText.text = content.contentText;
+                contentImage.SetEnabled(false);
+                break;
+            case Content.ContentType.ImagePost:
+                contentImage.image = content.contentImage;
+                contentText.enabled = false;
+                break;
+            default:
+                break;
+        }
+    }
 }
